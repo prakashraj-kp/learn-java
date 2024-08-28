fef90dee35f7ecdf6bc5d653ce53826ee62f8272
1410git <prakashraj2020.pk@gamil.com>
2024-08-29 04:56:43 +0530

    java variable

---
+++ variable.java
@@ -0,0 +1,10 @@
+/**variable :
+variable is container which hold data assigh with datatyupe ;
+example int a=10;  "a" is verial  */
+public class variable {
+    public static void main(String[] args) {
+        int a= 10,b=20,c=30;
+        System.out.println("a of value="+a+"\nb of value="+"\nc  of value="+c);
+        
+    }
+}

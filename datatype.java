b4155da8764a5b26bba624bd016347e318e82fe3
1410git <prakashraj2020.pk@gamil.com>
2024-08-29 05:28:29 +0530

    datatypes

---
+++ datatype.java
@@ -0,0 +1,32 @@
+/* Data type:
+             data type is used to allow sufficient memory space for  data.
+             "java is strongly type languge" because data must declare with data type.
+              there are two types
+              1.primitive datatype
+              2. non-primitive datatype
+               the primitive value store only one values and naver store mutiple values same of datatype. their values show below 
+
+               1.byte = 1 byte
+               2.short = 2 byte 
+               3.int  = 4 byte 
+               4. long = 8 byte 
+               5.float = 4 byte
+               6. double = 8 byte
+               7. char = 2 byte
+               8.boolean = 1 bit  */
+                                           
+              
+
+public class datatype {
+    public static void main(String[] args) {
+ byte a =10;
+ short b =20;
+ long c = 30;
+int d = 40;
+float e = 2.3f;
+double f = 23.00;
+char g ='+';
+boolean boy = true;
+ System.out.println("a of value="+a+"\n b of value="+b+"\n c of value="+c+"\n d of value="+d+"\n e of value="+e+"\n f of value="+f+"\nboy="+boy);
+    }
+}

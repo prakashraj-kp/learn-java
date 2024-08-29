e942fbdb21a5f3c8b6c53178dd634a9ca333c758
prakashraj-kp <151444126+prakashraj-kp@users.noreply.github.com>
2024-08-29 05:31:08 +0530

    Update variable.java

--- variable.java
+++ variable.java
@@ -1,6 +1,6 @@
 /**variable :
 variable is container which hold data assigh with datatyupe ;
-example int a=10;  "a" is verial  */
+example int a=10;  "a" is variable  */
 public class variable {
     public static void main(String[] args) {
         int a= 10,b=20,c=30;
